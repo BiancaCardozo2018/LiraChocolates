959104b9eeba5b82a52427ae6d412403e0a07477
Bianca Lira <bianca1999@gmail.com>
2018-06-26 11:57:42 -0300

    API- Criação da API de LiraChocolates 2

--- index.js
+++ index.js
@@ -127,5 +127,5 @@ app.delete('/brigadeiros/:id', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, ()=>console.log("Aplicação concluida!"));
+app.listen(process.env.PORT || 3000, () => console.log("Aplicação concluida!"));
 
